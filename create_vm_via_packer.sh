50ca5fe41b4d2c44e92e9a4dd4cdc0bae52e7128
dlovell <dlovell@gmail.com>
2013-10-12 23:06:34 -0400

    first cut at vm install scripts

---
+++ create_vm_via_packer.sh
@@ -0,0 +1,24 @@
+packer_config_filename=$1
+if [[ -z $packer_config_filename ]]; then
+	echo "USAGE: bash create_vm_via_packer.sh PACKER_CONFIG_FILENAME"
+	exit
+fi
+
+
+# packer presumes paths in config are relative to working directory
+# so cd to config filename location
+config_abs_path=$(readlink -f "$packer_config_filename")
+config_abs_dir=$(dirname $config_abs_path)
+cd $config_abs_dir
+
+
+# validate
+packer validate $packer_config_filename
+if [[ $? -ne 0 ]]; then
+	echo "Failed to validate $packer_config_filename"
+	exit
+fi
+
+
+# build
+export PACKER_LOG=1 && packer build $packer_config_filename >out 2>err
---
+++ install.sh
@@ -0,0 +1,16 @@
+# install packer on ubuntu
+which_packer=$(which packer)
+if [[ -z $which_packer ]]; then
+	install_dir=~/packer
+	which_zip=0.3.7_linux_amd64.zip
+	#
+	sudo apt-get install -y qemu-utils
+	cd $(dirname $install_dir)
+	wget https://dl.bintray.com/mitchellh/packer/$which_zip
+	mkdir $install_dir
+	cd $install_dir
+	unzip ../$which_zip
+	cat -- >> ~/.bashrc <<EOF
+	export PATH=\$PATH:$install_dir
+EOF
+fi
