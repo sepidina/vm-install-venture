42cfeb1f5b5142eb729a7d524cb6b2c9c3ca9184
dlovell <dlovell@gmail.com>
2013-10-14 12:37:23 -0400

    add virtualbox as requirement

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
+# ensure system requirements exist
+sudo apt-get install -y virtualbox qemu-utils 
+
+
 # install packer on ubuntu
 which_packer=$(which packer)
 if [[ -z $which_packer ]]; then
 	install_dir=~/packer
 	which_zip=0.3.7_linux_amd64.zip
 	#
-	sudo apt-get install -y qemu-utils
 	cd $(dirname $install_dir)
 	wget https://dl.bintray.com/mitchellh/packer/$which_zip
 	mkdir $install_dir
