48ba7e5203e1942e3f0f7c59ddac56bc1293c74b
dlovell <dlovell@gmail.com>
2013-10-23 11:25:41 -0400

    install some help packages; remove VBoxGuestAdditions.iso; clean up

--- provision.sh
+++ provision.sh
@@ -1,1 +1,7 @@
+apt-get install -y htop nmon tmux screen
+apt-get install -y git ack-grep
 apt-get install -y --force-yes vim-nox
+
+rm /home/bigdata/VBoxGuestAdditions.iso
+apt-get clean
+dd if=/dev/zero of=fillfile bs=1M || echo
