725ea9eb0c67cbeb0d7f207e795ba7b36bbefb87
dlovell <dlovell@gmail.com>
2013-11-04 09:08:25 -0500

    parse username from preseed file

--- settings.sh
+++ settings.sh
@@ -2,6 +2,6 @@ project_name=vm-install-archetype-virtualbox
 project_dir=$project_name
 ovf_full_path="${project_dir}"/"${project_name}".ovf
 port_number=2222
-username=bigdata
+username=$(grep '^d-i passwd/username string' ubuntu-12.04.2-server-preseed.cfg | awk '{print $NF}')
 rsa_key_filename=vm_guest_id_rsa
 packer_config_filename=vbox_ubuntu-12.04.2-server-amd64_packer_config.json
