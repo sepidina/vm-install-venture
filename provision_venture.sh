30b38f58dce5c89ac8e474cf9870868186b005cb
dlovell <dlovell@gmail.com>
2013-11-04 11:57:44 -0500

    BUGFIX: fix issue with g++ 4.8 install
    ordering of g++ 4.8 and other system dependencies matters

--- provision_venture.sh
+++ provision_venture.sh
@@ -1,3 +1,9 @@
+# get non C++11 system dependencies
+# MUST install these BEFORE gcc/g++ 4.8 install
+sudo apt-get install -y libboost1.48-all-dev
+sudo apt-get install -y cmake
+sudo apt-get install -y libgsl0-dev
+
 # get C++11
 sudo apt-get install -y python-software-properties
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
@@ -6,13 +12,6 @@ sudo apt-get install -y gcc-4.8
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 50
 sudo apt-get install -y g++-4.8
 sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 50
-# need to do it a second time?  Manual build of venture required rerun of g++ update-alternatives
-sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 50
-
-# get non C++11 system dependencies
-sudo apt-get install -y libboost1.48-all-dev
-sudo apt-get install -y cmake
-sudo apt-get install -y libgsl0-dev
 
 # get and build venture
 sudo apt-get install -y git make
