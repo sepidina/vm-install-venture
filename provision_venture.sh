83255cbda9b34188ff899be79e53b4f6568e2faf
dlovell <dlovell@gmail.com>
2013-11-04 20:59:34 -0500

    consolidate apt-get operations

--- provision_venture.sh
+++ provision_venture.sh
@@ -1,20 +1,16 @@
 # get non C++11 system dependencies
 # MUST install these BEFORE gcc/g++ 4.8 install
-sudo apt-get install -y libboost1.48-all-dev
-sudo apt-get install -y cmake
-sudo apt-get install -y libgsl0-dev
+sudo apt-get install -y libboost1.48-all-dev libgsl0-dev cmake make git
 
 # get C++11
 sudo apt-get install -y python-software-properties
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt-get update
-sudo apt-get install -y gcc-4.8
+sudo apt-get install -y gcc-4.8 g++-4.8
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 50
-sudo apt-get install -y g++-4.8
 sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 50
 
 # get and build venture
-sudo apt-get install -y git make
 # git clone https://github.com/mit-probabilistic-computing-project/venturelite
 # cd venturelite && git checkout cxx
 # cmake . && make
