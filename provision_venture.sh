a1a77504e1fb461a42ade0bd1577c23fe9f4364e
dlovell <dlovell@gmail.com>
2013-11-09 00:42:38 -0500

    update provisioning for "python setup.py install", venture_unit requirements

--- provision_venture.sh
+++ provision_venture.sh
@@ -1,6 +1,9 @@
 # get non C++11 system dependencies
 # MUST install these BEFORE gcc/g++ 4.8 install
-sudo apt-get install -y libboost1.48-all-dev libgsl0-dev cmake make git
+sudo apt-get build-dep -y python-numpy python-matplotlib
+sudo apt-get install -y libboost1.48-all-dev libgsl0-dev cmake make git python-pip python-virtualenv ccache
+# must update distribute before requirements.txt install
+sudo pip install -U distribute
 
 # get C++11
 sudo apt-get install -y python-software-properties
@@ -11,6 +14,7 @@ sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 50
 sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 50
 
 # get and build venture
-# git clone https://github.com/mit-probabilistic-computing-project/venturelite
-# cd venturelite && git checkout cxx
-# cmake . && make
+# git clone https://github.com/mit-probabilistic-computing-project/Venturecxx
+cd Venturecxx
+sudo pip install -r requirements.txt
+python setup.py install
