f02b01bc4babe5e13731e725e7eaa0f0539bf3f4
Alexey Radul <axch@mit.edu>
2013-11-20 11:38:03 -0500

    Actually, VirtualBox does not operate on VMs in place, but copies them to its own space.  Therefore, the proper process is not copy and modify but import modify and export.

--- make-venture-vm-via-packer.sh
+++ make-venture-vm-via-packer.sh
@@ -9,14 +9,10 @@ if [[ $? -ne "0" ]]; then
 	exit 1
 fi
 
-# Copy it into the working location
-rm -rf $project_dir
-cp -r base-vm $project_dir
-
 # Import it into VitrualBox
 # must be NAT on VM creation
 # must specify not to change nat mac; else mac changes, eth0 doesn't come up and can't get back in via ssh
-VBoxManage import $ovf_full_path --options keepnatmacs
+VBoxManage import "base-vm/${project_name}.ovf" --options keepnatmacs
 
 # Boot it
 VBoxManage startvm "${project_name}" --type headless
@@ -28,6 +24,9 @@ bash install-venture-into-vm.sh
 ssh -i $rsa_key_filename -p $port_number -o StrictHostKeyChecking=no $username@localhost "echo $userpass | sudo -S halt"
 sleep 30 # Is this enough time for the thing to shut down properly?
 VBoxManage controlvm "${vmname}" poweroff
+
+# Export it
+VBoxManage export "${vmname}" --output $ovf_full_path
 
 # Get rid of it
 VBoxManage unregistervm "${vmname}" --delete
