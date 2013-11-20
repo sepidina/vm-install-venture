7c89adebb91bb05738c10f4c0ba037e78ebfaf89
Alexey Radul <axch@mit.edu>
2013-11-20 13:45:01 -0500

    Right.  The vmname variable should actually exist.

--- settings.sh
+++ settings.sh
@@ -5,6 +5,7 @@ port_number=2222
 username=$(grep '^d-i passwd/username string' ubuntu-12.04.2-server-preseed.cfg | awk '{print $NF}')
 password=$username
 rsa_key_filename=vm_guest_id_rsa
+vmname=$project_name
 
 function abort_on_error () {
     if [[ $? -ne "0" ]]; then
