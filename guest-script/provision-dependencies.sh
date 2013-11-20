78532bfc33c2a0976c1b5e47f55bdc5523694ab2
Alexey Radul <axch@mit.edu>
2013-11-20 12:06:07 -0500

    This should produce the desired error behavior in provisioning dependencies.

--- guest-script/provision-dependencies.sh
+++ guest-script/provision-dependencies.sh
@@ -1,15 +1,15 @@
 # get non C++11 system dependencies
 # MUST install these BEFORE gcc/g++ 4.8 install
-sudo apt-get update
-sudo apt-get build-dep -y python-numpy python-matplotlib
-sudo apt-get install -y libboost1.48-all-dev libgsl0-dev cmake make git python-pip python-virtualenv ccache
+sudo apt-get update && \
+sudo apt-get build-dep -y python-numpy python-matplotlib && \
+sudo apt-get install -y libboost1.48-all-dev libgsl0-dev cmake make git python-pip python-virtualenv ccache && \
 # must update distribute before requirements.txt install
-sudo pip install -U distribute
+sudo pip install -U distribute && \
 
 # get C++11
-sudo apt-get install -y python-software-properties
-sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
-sudo apt-get update
-sudo apt-get install -y gcc-4.8 g++-4.8
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 50
+sudo apt-get install -y python-software-properties && \
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test && \
+sudo apt-get update && \
+sudo apt-get install -y gcc-4.8 g++-4.8 && \
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 50 && \
 sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 50
