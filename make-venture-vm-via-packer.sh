82f68d4a5daac50e13c6d16c10b008bbc4c52198
Alexey Radul <axch@mit.edu>
2013-11-20 13:46:52 -0500

    Look for name collisions among importable VMs and stomp the old one.

--- make-venture-vm-via-packer.sh
+++ make-venture-vm-via-packer.sh
@@ -6,7 +6,22 @@ source ./settings.sh
 make base-vm
 abort_on_error "make base-vm"
 
-# Import it into VitrualBox
+# If a VM with the name we want already exists, assume it is dirty and
+# get rid of it
+VBoxManage list runningvms | grep -q $vmname
+if [[ $? -eq "0" ]]; then
+    VBoxManage controlvm $vmname poweroff
+    sleep 10 # How long is enough time?
+fi
+
+VBoxManage list vms | grep -q $vmname
+if [[ $? -eq "0" ]]; then
+    echo "Unregistering extant VM named ${vmname}."
+    VBoxManage unregistervm $vmname --delete
+    abort_on_error "Unregistering extant VM named ${vmname}."
+fi
+
+# Import the base VM into VitrualBox
 # must be NAT on VM creation
 # must specify not to change nat mac; else mac changes, eth0 doesn't come up and can't get back in via ssh
 VBoxManage import "base-vm/${project_name}.ovf" --options keepnatmacs
