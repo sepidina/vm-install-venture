7ee0a1a695e8eb5cd5bf23fc9d719de8ec0438aa
Alexey Radul <axch@mit.edu>
2013-11-20 13:28:15 -0500

    Right.  Actually pass the proper password to sudo.

--- settings.sh
+++ settings.sh
@@ -3,6 +3,7 @@ project_dir=$project_name
 ovf_full_path="${project_dir}"/"${project_name}".ovf
 port_number=2222
 username=$(grep '^d-i passwd/username string' ubuntu-12.04.2-server-preseed.cfg | awk '{print $NF}')
+password=$username
 rsa_key_filename=vm_guest_id_rsa
 
 function abort_on_error () {
