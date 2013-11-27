cfb8517e628b45987221a06aeef9c89ba7c9e02c
Alexey Radul <axch@mit.edu>
2013-11-27 14:00:56 -0500

    I lost my cached base VM anyway, so add dependencies to the provisioner.

--- guest-script/install-venture.sh
+++ guest-script/install-venture.sh
@@ -17,9 +17,6 @@ fi
 
 # Build Venture
 cd Venturecxx
-# TODO These are sufficiently slow and reliable to be moved into
-# provision-dependencies.sh, but I do not want to invalidate my cached
-# base-vm right now.
 echo "venture" | sudo -S -p "" pip install -r requirements.txt
 abort_on_error "Installing Python dependencies."
 echo "venture" | sudo -S -p "" python setup.py install
--- guest-script/provision-dependencies.sh
+++ guest-script/provision-dependencies.sh
@@ -1,7 +1,6 @@
 # get non C++11 system dependencies
 # MUST install these BEFORE gcc/g++ 4.8 install
 sudo apt-get update && \
-sudo apt-get build-dep -y python-numpy python-matplotlib && \
 sudo apt-get install -y libboost1.48-all-dev libgsl0-dev cmake make git python-pip python-virtualenv ccache && \
 # must update distribute before requirements.txt install
 sudo pip install -U distribute && \
@@ -13,3 +12,6 @@ sudo apt-get update && \
 sudo apt-get install -y gcc-4.8 g++-4.8 && \
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 50 && \
 sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 50
+
+# Python dependencies
+sudo apt-get install -y python-pyparsing python-flask python-requests python-numpy python-matplotlib
